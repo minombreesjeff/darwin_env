aa0798e613aace82ab0c8f84afbfb1095911c813
IceClown Wasp <gendo5150@gmail.com>
2018-05-15 17:27:16 +0200

    libarchive-23

--- libarchive/libarchive/cpio/test/test_option_L.c
+++ libarchive/libarchive/cpio/test/test_option_L.c
@@ -23,62 +23,28 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #include "test.h"
-__FBSDID("$FreeBSD: src/usr.bin/cpio/test/test_option_L.c,v 1.2 2008/08/24 06:21:00 kientzle Exp $");
+__FBSDID("$FreeBSD$");
 
-DEFINE_TEST(test_option_L)
+DEFINE_TEST(test_option_l)
 {
-	struct stat st;
-	int fd, filelist;
 	int r;
 
-	filelist = open("filelist", O_CREAT | O_WRONLY, 0644);
+	/* Create a file. */
+	assertMakeFile("f", 0644, "a");
 
-	/* Create a file and a symlink to the file. */
-	fd = open("file", O_CREAT | O_WRONLY, 0644);
-	assert(fd >= 0);
-	assertEqualInt(10, write(fd, "123456789", 10));
-	close(fd);
-	write(filelist, "file\n", 5);
-
-	/* Symlink to above file. */
-	assertEqualInt(0, symlink("file", "symlink"));
-	write(filelist, "symlink\n", 8);
-
-	close(filelist);
-
-	r = systemf("cat filelist | %s -pd copy >copy.out 2>copy.err", testprog);
-	assertEqualInt(r, 0);
-	assertEqualInt(0, lstat("copy/symlink", &st));
-	failure("Regular -p without -L should preserve symlinks.");
-	assert(S_ISLNK(st.st_mode));
-
-	r = systemf("cat filelist | %s -pd -L copy-L >copy-L.out 2>copy-L.err", testprog);
-	assertEqualInt(r, 0);
-	assertEmptyFile("copy-L.out");
-	assertFileContents("1 block\n", 8, "copy-L.err");
-	assertEqualInt(0, lstat("copy-L/symlink", &st));
-	failure("-pdL should dereference symlinks and turn them into files.");
-	assert(!S_ISLNK(st.st_mode));
-
-	r = systemf("cat filelist | %s -o >archive.out 2>archive.err", testprog);
-	failure("Error invoking %s -o ", testprog);
+	/* Copy the file to the "copy" dir. */
+	r = systemf("echo f | %s -pd copy >copy.out 2>copy.err",
+	    testprog);
 	assertEqualInt(r, 0);
 
-	assertEqualInt(0, mkdir("unpack", 0755));
-	r = systemf("cat archive.out | (cd unpack ; %s -i >unpack.out 2>unpack.err)", testprog);
-	failure("Error invoking %s -i", testprog);
-	assertEqualInt(r, 0);
-	assertEqualInt(0, lstat("unpack/symlink", &st));
-	assert(S_ISLNK(st.st_mode));
+	/* Check that the copy is a true copy and not a link. */
+	assertIsNotHardlink("f", "copy/f");
 
-	r = systemf("cat filelist | %s -oL >archive-L.out 2>archive-L.err", testprog);
-	failure("Error invoking %s -oL", testprog);
+	/* Copy the file to the "link" dir with the -l option. */
+	r = systemf("echo f | %s -pld link >link.out 2>link.err",
+	    testprog);
 	assertEqualInt(r, 0);
 
-	assertEqualInt(0, mkdir("unpack-L", 0755));
-	r = systemf("cat archive-L.out | (cd unpack-L ; %s -i >unpack-L.out 2>unpack-L.err)", testprog);
-	failure("Error invoking %s -i < archive-L.out", testprog);
-	assertEqualInt(r, 0);
-	assertEqualInt(0, lstat("unpack-L/symlink", &st));
-	assert(!S_ISLNK(st.st_mode));
+	/* Check that this is a link and not a copy. */
+	assertIsHardlink("f", "link/f");
 }
