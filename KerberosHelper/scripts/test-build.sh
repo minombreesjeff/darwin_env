d9e7d93c8b3d77bb51edf50ff0f8ea62011d7ed5
IceClown Wasp <gendo5150@gmail.com>
2018-07-22 09:06:57 +0200

    KerberosHelper-151

--- KerberosHelper/scripts/test-build.sh
+++ KerberosHelper/scripts/test-build.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
 
-release=OSX108
-
-buildit . -project=KerberosHelper -release $release -rootsDirectory $HOME/BuildRoots -arch i386 -arch x86_64 -merge /  "$@"
-buildit . -project=KerberosHelper_executables -target KerberosHelper_executables -release $release -rootsDirectory $HOME/BuildRoots -arch x86_64 -merge /  "$@"
-
